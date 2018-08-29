fd35784e286e9c6d42325d5163c85365d2df3a6c
Alexander Roikier Møller <alex6308@elev.tec.dk>
2018-08-29 14:37:12 +0200

    <Mangler halvdelen>

---
+++ OOP-Klassen-Motorcycle/OOP-Klassen-Motorcycle/Motorcycle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Klassen_Motorcycle
+{
+    class Motorcycle
+    {
+        public bool Started;
+        public int Rpm;
+        public int Gear;
+
+        public string Name { get; set; }
+
+        public Motorcycle()
+        {
+            Started = false;
+            Rpm = 0;
+            Gear = 0;
+            Name = "";
+        }
+
+        public Motorcycle(bool Started, int Rpm, int Gear, string Name)
+        {
+            Started = true;
+            Rpm = 1000;
+            Gear = 0;
+            Name = "";
+        }
+    }
+}
